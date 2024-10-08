b8f9517b9b4538f321eec5ecb7e153971ffabfb7
jainsahabtusharjain <102910019+jainsahabtusharjain@users.noreply.github.com>
2024-10-08 07:34:01 +0000

    New commit on 8-10-24

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useTranslation } from 'react-i18next'
+import Languageselector from './components/language-selector'
 import './App.css'
 
+
 function App() {
+  const {t} = useTranslation()
+  const description =  t('description')
+  console.log(description)
   return(
     <div className='container'>
-      <h1>Hello welcome</h1>
+      <Languageselector />
+      <h1>{t("greetings")}</h1>
     </div>
   )
 }
