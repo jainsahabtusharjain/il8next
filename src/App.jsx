ff84de80aad35ffd1feb7303fe4967be348f4d88
jainsahabtusharjain <102910019+jainsahabtusharjain@users.noreply.github.com>
2024-10-10 10:04:32 +0000

    smallchange

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 
 function App() {
   const {t} = useTranslation()
-  const description =  t('description')
-  console.log(description)
+  const {line1, line2} =  t('description')
+  // console.log(description)
   return(
     <div className='container'>
       <Languageselector />
       <h1>{t("greetings")}</h1>
+      <p>{line1}</p>
+      <span>{line2}</span>
     </div>
   )
 }
