ca257f19a0f1bd725c09f93f23dcb8d0846ad6d0
jainsahabtusharjain <102910019+jainsahabtusharjain@users.noreply.github.com>
2024-10-14 13:02:07 +0000

    translationdone

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import Languageselector from './components/language-selector'
 import './App.css'
 
@@ -6,12 +6,23 @@ import './App.css'
 function App() {
   const {t} = useTranslation()
   console.log(t('description'))
-  const {line1, line2} =  t('description')
+  const {line1, line2} =  t('description', {
+    proJectname : "i18next "
+  })
   return(
     <div className='container'>
       <Languageselector />
       <h1>{t("greetings")}</h1>
-      <p>{line1}</p>
+      <span>
+        <Trans 
+          i18nKey = {line1}
+          values = {{
+            proJectname : "i18next",
+          }}
+          components = {{1: <b />}}
+          />
+      </span>
+      {/* <p>{line1}</p> */}
       <span>{line2}</span>
     </div>
   )
--- src/i18n.js
+++ src/i18n.js
@@ -1,47 +1,11 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend';
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
+
+i18n.use(LanguageDetector).use(initReactI18next).use(Backend).init({
     debug:true,
     fallbackLng: 'en',
     returnObjects : true,
-    resources:{
-        en: {
-            translation:{
-                greetings: "Hello Welcome !",
-                description: {
-                    line1: "Hello every one we are doinig a dummy project",
-                    line2: "this is a second line"
-                }
-            }
-        },
-        fr: {
-            translation:{
-                greetings: "Bonjour Bienvenue",
-                description: {
-                    line1: "Bonjour à tous, nous faisons un projet factice",
-                    line2: "c'est une deuxième ligne"
-                }
-            }
-        },
-        hi:{
-            translation:{
-                greetings: "नमस्ते, आपका स्वागत है",
-                description: {
-                    line1: "सभी को नमस्कार, हम एक डमी प्रोजेक्ट पर काम कर रहे हैं",
-                    line2: "यह दूसरी पंक्ति है"
-                }
-            }
-        },
-        ar:{
-            translation:{
-                greetings: "مرحبا، مرحبا",
-                description: {
-                    line1: "مرحبا بالجميع، نحن نعمل على مشروع وهمي",
-                    line2: "هذا هو السطر الثاني"
-                }
-            }
-        },
-    }
 })
